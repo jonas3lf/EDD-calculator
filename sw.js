a13eda65e5c4caab2fed3d0d67e9c9105ec65c3c
jonas3lf <jonas.3lf@gmail.com>
2026-04-05 08:14:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,29 @@
-const CACHE = "ivf-v1";
+const CACHE_NAME = 'ivf-calc-v2'; // Změň verzi (v1 -> v2) při každé velké změně
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // Vynutí okamžitou aktivaci nové verze
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
